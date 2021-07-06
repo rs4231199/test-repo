2c85ff37c8e94003d3062c6b9a81e30db76539a6
Rahul Saini <rs4231199@gmail.com>
2021-07-06 10:45:19 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+	ll x = 34;
+	cin >> x;
+	if (x != 34) {
+		cout << "haha! Wrong.\n";
+	} else {
+		cout << "yay!! Correct.\n";
+	}
+	
+	return 0;
+}
